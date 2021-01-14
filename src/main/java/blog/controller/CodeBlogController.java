fb638c3ce8069836d7925ca125384146a4c4f8d3
Ruan-_pb <ruancruzsoares@hotmail.com>
2021-01-14 15:22:28 -0300

    renderizando paginas e fazendo formulario

--- src/main/java/blog/controller/CodeBlogController.java
+++ src/main/java/blog/controller/CodeBlogController.java
@@ -1,30 +1,68 @@
 package blog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import blog.model.Post;
 import blog.services.PostService;
 
-@RestController
+@Controller
 public class CodeBlogController {
 	
 	@Autowired
 	PostService codeBlogService;
 	
 	
-	@RequestMapping(value = "/posts",method = RequestMethod.GET)
-	public ModelAndView getPosts() {
-		ModelAndView mv = new ModelAndView("posts");
-		List<Post> posts = codeBlogService.findAll();
-		mv.addObject("posts", posts);
+	@RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value="/posts", method=RequestMethod.GET)
+    public ModelAndView getPosts(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeBlogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+	@RequestMapping(value = "/posts/{id}",method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = codeBlogService.findById(id);
+		mv.addObject("post", post);
 		return mv;
 	}
+	@RequestMapping(value = "/newpost",method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost",method = RequestMethod.POST)
+	public String savePost(@Valid Post post,BindingResult result,RedirectAttributes attributes ) {
+		if(result.hasErrors()) {
+			 attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeBlogService.save(post);
+		return "redirect:/posts";
+		
+		
+	}
+	
+	
+	
+	
 
 }
